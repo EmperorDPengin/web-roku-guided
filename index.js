98389776e7bac4200921f4b5eb9baad3f76abbac
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-03-10 18:15:21 -0500

    first staging after eslint

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const server = express();
+const PORT = 9000;
+
+server.get('/', (req, res) => {
+    res.end('<h1>hello world</h1>');
+});
+
+server.listen(PORT, () => {
+    console.log(`server is up on Port ${PORT}`);
+});
